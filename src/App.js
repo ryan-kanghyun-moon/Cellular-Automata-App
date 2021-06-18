319295f301c1e71c215ec0d5fc15d918e26e5796
ryan-kanghyun-moon <kanghyun323@gmail.com>
2021-06-18 12:36:08 -0700

    add grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const numRows = 120;
+const numCols = 120;
+const pxSize = 8;
 
 function App() {
+  const arr = Array(numRows).fill(Array(numCols).fill(0));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, ${pxSize + 2}px)`
+      }}>
+      {arr.map((row, i) => (
+          row.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              style={{
+                width: pxSize,
+                height: pxSize,
+                backgroundColor: "green",
+                border: "solid 1px black"
+              }}
+            />
+          ))
+      ))}
     </div>
   );
 }
