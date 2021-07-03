56d797dc958ac89d4ad27e563f99e5208b26b1ae
ryan-kanghyun-moon <kanghyun323@gmail.com>
2021-07-03 01:04:26 -0700

    finish frame work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom'
-const numRows = 110;
-const numCols = 110;
+const numRows = 100;
+const numCols = 100;
 const pxSize = 8;
+const buttonSize = 20;
 
 const rules = {
   maze : {
@@ -15,23 +16,111 @@ const rules = {
     live : [0, 0, 0, 1, 0, 0, 0, 0, 0],
   },
 
-  life : {
+  'game of life' : {
     surv : [0, 0, 1, 1, 0, 0, 0, 0, 0],
     live : [0, 0, 0, 1, 0, 0, 0, 0, 0],
   },
-}
+};
+
+const ruleList = [
+  'maze', 'mazectric', 'game of life', 'free',
+];
 
 function CAMazeSelector(props) {
-  // TODO
   return (
-    <div>selector</div>
+    <div style={{display:"flex"}}>
+      <div 
+        classID={"CAOption"}
+        style={{
+          width: 130,
+          height: 20,
+          backgroundColor: props.isCA ? "pink" : "white",
+          border: "solid 1px black"
+        }}
+        onClick={() => props.setIsCA(!props.isCA)}
+      >Cellular Automata</div>
+      <div 
+        classID={"MazeOption"}
+        style={{
+          width: 130,
+          height: 20,
+          backgroundColor: !props.isCA ? "pink" : "white",
+          border: "solid 1px black"
+        }}
+        onClick={() => props.setIsCA(!props.isCA)}
+      >Maze</div>
+    </div>
   )
 }
 
 function CAPanel(props) {
-  // TODO
+  
   return (
-    <div>CA panel</div>
+    <div>
+      <form>
+        <label>
+          Select rule 
+          <select value={props.currRule} onChange={(event) => {
+            const val = event.target.value;
+            props.setCurrRule(val);
+            if (val !== 'free') {
+            props.setLive(rules[val]['live']);
+            props.setSurv(rules[val]['surv']);
+          } else {
+            props.setLive(new Array(9).fill(0));
+            props.setSurv(new Array(9).fill(0));
+          }}}>
+            {ruleList.map((rule) => <option value={rule} key={`${rule}`}>{rule}</option>)}
+          </select>
+        </label>
+      </form>
+      <div> 
+        <h3>A cell will come to life with</h3>
+        <div style={{display: "flex"}}>
+          {props.live.map((val, i) => 
+            <div key={i} 
+              className='botton'
+              style={{
+                height: buttonSize, 
+                width: buttonSize, 
+                backgroundColor: val ? "lightgreen" : "white",
+                border: "solid 1px black"}}
+                onClick={() => {
+                if (props.currRule === 'free') {
+                  var newLive = [...props.live];
+                  newLive[i] = !props.live[i];
+                  props.setLive(newLive);
+                }
+              }}>
+            {i}
+            </div>)}
+        </div>
+        <h3>living neighbors,</h3>
+        <h3>and survive with</h3>
+        <div style={{display: "flex"}}>
+          {props.surv.map((val, i) => 
+            <div key={i} 
+              className='botton'
+              style={{
+                height: buttonSize, 
+                width: buttonSize, 
+                backgroundColor: val ? "skyblue" : "white",
+                border: "solid 1px black"}}
+              onClick={() => {
+                if (props.currRule === 'free') {
+                  var newSurv = [...props.surv];
+                  newSurv[i] = !props.surv[i];
+                  props.setSurv(newSurv);
+                 }
+                }}>
+            {i}
+            </div>)}
+        </div>
+        <h3>living neighbors,</h3>
+        <h3>or be dead otherwise.</h3>
+        
+      </div>
+    </div>
   )
 }
 
@@ -39,7 +128,7 @@ function MazePanel(props) {
   //TODO
   return (
     <div>
-      Maze panel
+      Maze panel - coming very very soon!
     </div>
   )
 }
@@ -77,6 +166,10 @@ function Board(props) {
   );
 }
 
+function Spacer() {
+  return <div style={{width: 20, height: 20}}/>
+}
+
 function Game() {
   var [grid, setGrid] = useState(() => Array(numRows).fill().map(() => new Array(numCols).fill(0)));
   var [surv, setSurv] = useState(rules.maze.surv);
@@ -90,44 +183,59 @@ function Game() {
   
   return (
     <div>
-      <CAMazeSelector isCA={isCA} setIsCA={setIsCA}/>
-      <Board grid={grid} setGrid={setGrid} rule={currRule} isOn={isOn}/>
-      
-      <div>
-        {isCA ?  
-          <CAPanel rule={currRule} setRule={setCurrRule} surv={surv} live={live}/>
-          :
-          <MazePanel/>}
+      <div style={{display:"flex"}}>
+        <Board grid={grid} setGrid={setGrid} rule={currRule} isOn={isOn}/>
+       
+        <Spacer/>
+
+        <div>
+          <CAMazeSelector isCA={isCA} setIsCA={setIsCA}/>
+
+          <Spacer/>
+          
+          <div>
+            {isCA ?  
+              <CAPanel currRule={currRule} setCurrRule={setCurrRule} surv={surv} setSurv={setSurv} live={live} setLive={setLive}/>
+              :
+              <MazePanel/>}
+          </div>
+
+          <Spacer/>
+
+          <button classID="playPauseButton" onClick={() => {setIsOn(!isOn)}}>
+            {!isOn ? "play" : "pause"}
+          </button>
+          
+          <button classID="resetButton" onClick={() => {
+            setGrid(() => Array(numRows).fill().map(() => new Array(numCols).fill(0)));
+            setIsOn(false);}}>
+            {"reset"}
+          </button>
+
+        </div>
       </div>
-      
-      <button classID="playPauseButton" onClick={() => {setIsOn(!isOn)}}>
-        {!isOn ? "play" : "pause"}
-      </button>
-      
-      <button classID="resetButton" onClick={() => {
-        setGrid(() => Array(numRows).fill().map(() => new Array(numCols).fill(0)));
-        setIsOn(false);}}>
-        {"reset"}
-      </button>
 
-      <form>
-        <label>
-          random level (0 - 1):
-          <input type="text" value={randLevel} onChange={
-            (event) => 
-              {let value = event.target.value;
-                Number(value) == value && value >= 0 && value <= 1 ? 
-                setRandLevel(event.target.value) : setRandLevel(randLevel)}
-          }/>
-        </label>
-      </form>
-      <button onClick={() => {
-        var newArr = new Array(numRows).fill().map(
-          () => new Array(numCols).fill().map(
-            () => Math.random() < randLevel ? 1 : 0));
-        setGrid(newArr);
-      }}>randomize!</button>
+      <Spacer/>
 
+      <div style={{display: "flex"}}>
+          <form>
+            <label>
+              random level (0 - 1): 
+              <input type="text" value={randLevel} onChange={
+                (event) => 
+                  {let value = event.target.value;
+                    Number(value) == value && value >= 0 && value <= 1 ? 
+                    setRandLevel(event.target.value) : setRandLevel(randLevel)}
+              }/>
+            </label>
+          </form>
+          <button onClick={() => {
+            var newArr = new Array(numRows).fill().map(
+              () => new Array(numCols).fill().map(
+                () => Math.random() < randLevel ? 1 : 0));
+            setGrid(newArr);
+          }}>randomize!</button>
+      </div>
     </div>
   ) 
 }
